9f7d3eb4df012282686f73a6e578936c53d58130
martinstrn <martin.sterin1@gmail.com>
2024-01-16 16:36:41 +0100

    version avec le bon nom

--- test_sorting.py
+++ test_sorting.py
@@ -1,9 +1,9 @@
 import sorting.py
 
-def test_sort():
-    assert sorting.sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
-    assert sorting.sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
-    assert sorting.sort([1, 3, 2, 5, 4]) == [1, 2, 3, 4, 5]
+def test_quick_sort():
+    assert sorting.quick_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert sorting.quick_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+    assert sorting.test_quick_sort([1, 3, 2, 5, 4]) == [1, 2, 3, 4, 5]
     assert([])==[]
     assert([1])==[1]
     assert([1,2])==[1,2]
